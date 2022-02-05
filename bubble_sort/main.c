acb116df79aa133d12f1a53221940f1c6f3ada91
Baek Han <han.baek.program@gmail.com>
2022-02-05 11:43:18 -0800

    Add bubble sort with flag

--- bubble_sort/main.c
+++ bubble_sort/main.c
@@ -27,15 +27,54 @@ void bubble_sort_noflag(int record[], int n) {
     printf("####################################\n");
 }
 
-int main(int argc, const char * argv[]) {
-    int record[5] = {9, 11, 4, 7, 8};
+void bubble_sort_flag(int record[], int n) {
+    printf("##### Bubble sort with no flag #####\n");
+    int i = 0, j, flag, temp;
     
-    printf("Initial Records: ");
+    flag = 1;
+    while (flag > 0 && i <= (n - 1)) {
+        printf("%d loop: ", (i + 1));
+        flag = 0;
+        for (j = 0; j < ((n - 1) - i); j++) {
+            if (record[j] > record[j + 1]) {
+                temp = record[j];
+                record[j] = record[j + 1];
+                record[j + 1] = temp;
+                flag = 1;
+            }
+        }
+        for (int i = 0; i < 5; i++) {
+            printf("%d ", record[i]);
+        }
+        printf(", flag = %d\n", flag);
+        i++;
+    }
+    
+    printf("Result: ");
     for (int i = 0; i < 5; i++) {
         printf("%d ", record[i]);
     }
     printf("\n");
-    bubble_sort_noflag(record, sizeof(record) / 4);
+    printf("####################################\n");
+}
+
+int main(int argc, const char * argv[]) {
+    int record1[5] = {9, 11, 4, 7, 8};
+    printf("Initial Records: ");
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", record1[i]);
+    }
+    printf("\n");
+    bubble_sort_noflag(record1, sizeof(record1) / 4);
+    printf("\n");
+    
+    int record2[5] = {9, 11, 4, 7, 8};
+    printf("Initial Records: ");
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", record2[i]);
+    }
+    printf("\n");
+    bubble_sort_flag(record2, sizeof(record2) / 4);
     
     return 0;
 }
